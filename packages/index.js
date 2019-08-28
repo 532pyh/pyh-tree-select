e6fcbe23c4aab6e7a5a8e8f272aa988498bb6e13
pengyh <l>
2019-08-28 16:55:37 +0800

    v0.5.0格式化文件

--- packages/index.js
+++ packages/index.js
@@ -5,15 +5,15 @@ const components = [
 ]
 
 const install = function (Vue) {
-  if (install.installed) return
-  components.map(component => Vue.component(component.name, component))
+    if (install.installed) return
+    components.map(component => Vue.component(component.name, component))
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+    install(window.Vue)
 }
 
 export default {
-  install,
-  treeSelect
+    install,
+    treeSelect
 }
